a729da9674cb2a3604fc4c13032c483ba93ad007
AllenTsai1215 <144758485+AllenTsai1215@users.noreply.github.com>
2024-10-17 16:36:23 +0800

    week06

---
+++ week06/week06-2a.cpp
@@ -0,0 +1,11 @@
+//week06-2a.cpp  step01 02
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	int now;
+	while(cin >> now){//step01 input
+		cout << now <<"\n";//step02 output
+	}
+}
